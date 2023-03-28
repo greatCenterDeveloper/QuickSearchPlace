db38a4ef1fd7789e1c94a5edcc8be8ed1d48cb9d
swj2023 <qkedkfkvkd@gmail.com>
2023-03-28 10:28:22 +0900

    패키지 분리

--- app/src/main/java/com/swj/quicksearchplace/activities/MainActivity.kt
+++ app/src/main/java/com/swj/quicksearchplace/activities/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.swj.quicksearchplace
+package com.swj.quicksearchplace.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.swj.quicksearchplace.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
