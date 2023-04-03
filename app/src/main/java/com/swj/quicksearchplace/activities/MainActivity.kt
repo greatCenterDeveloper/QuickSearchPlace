75dec479e794989e431f9cb12cc0159eb25a094f
swj2023 <qkedkfkvkd@gmail.com>
2023-04-03 11:00:14 +0900

    MainActivity toolbar 작업

--- app/src/main/java/com/swj/quicksearchplace/activities/MainActivity.kt
+++ app/src/main/java/com/swj/quicksearchplace/activities/MainActivity.kt
@@ -2,12 +2,35 @@ package com.swj.quicksearchplace.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.swj.quicksearchplace.R
+import com.swj.quicksearchplace.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding:ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        // 작업
+        // 툴바를 제목줄로 대체 - 옵션 메뉴랑 연결되도록
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_aa -> Toast.makeText(this, "aa", Toast.LENGTH_SHORT).show()
+            R.id.menu_bb -> Toast.makeText(this, "bb", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
